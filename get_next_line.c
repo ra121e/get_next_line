fe83632b50e5a90bb7c5851201e49c252ada5411
root <sushi.honda@gmail.com>
2024-06-08 10:08:21 +0800

    ???

---
+++ get_next_line.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 12:00:36 by athonda           #+#    #+#             */
+/*   Updated: 2024/06/08 10:08:03 by athonda          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file get_next_line.c
+ * @brief write a function that returns a line read from a file descriptor
+ */
+
+#include "get_next_line.h"
+
+/**
+ * @fn char	*get_next_line(int fd)
+ * @brief
+ * @param[in]	fd integer file descriptor
+ */
+
+int	main(void)
+{
+	return (0);
+}
