9e4f2d08245885e167f7991e44d681468d0eab17
root <sushi.honda@gmail.com>
2024-06-08 16:20:36 +0800

    test for open, read: OK

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 12:00:36 by athonda           #+#    #+#             */
-/*   Updated: 2024/06/08 10:08:03 by athonda          ###   ########.fr       */
+/*   Updated: 2024/06/08 16:19:26 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,31 @@
 /**
  * @fn char	*get_next_line(int fd)
  * @brief
- * @param[in]	fd integer file descriptor
+ * @param[in]	fd file descriptor
  */
+
+char	*get_next_line(int fd, char *p)
+{
+	char	buf[1024];
+
+	read(fd, buf, 100);
+	p = buf;
+	return (p);
+}
 
 int	main(void)
 {
+	int	fd;
+	char	*p;
+
+	fd = open("./test_open.txt", 0);
+	if (fd == -1)
+	{
+		printf("file open error!\n");
+		return (0);
+	}
+	p = get_next_line(fd, p);
+	write(1, p, 100);
+	close(fd);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,14 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 11:59:55 by athonda           #+#    #+#             */
-/*   Updated: 2024/06/07 11:59:59 by athonda          ###   ########.fr       */
+/*   Updated: 2024/06/08 15:58:16 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+#endif // GET_NEXT_LINE_H
