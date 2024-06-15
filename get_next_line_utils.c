e1a74e884d252cd554933f25f91ce4139242234c
root <sushi.honda@gmail.com>
2024-06-15 09:42:05 +0800

    customize: substr, strjoin for memcpy

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 11:15:45 by athonda           #+#    #+#             */
-/*   Updated: 2024/06/14 16:43:58 by athonda          ###   ########.fr       */
+/*   Updated: 2024/06/15 09:39:33 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,30 +22,13 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	unsigned char	*d;
-	unsigned char	*s;
-	size_t			i;
-
-	if (!dest && !src && n != 0)
-		return (NULL);
-	d = (unsigned char *)dest;
-	s = (unsigned char *)src;
-	i = 0;
-	while (i < n)
-	{
-		*(d + i) = *(s + i);
-		i++;
-	}
-	return (dest);
-}
-
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	len_s1;
 	size_t	len_s2;
 	char	*ptr;
+	int		i;
+	int		j;
 
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
@@ -54,8 +37,13 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	ptr = (char *)malloc(sizeof (char) * (len_s1 + len_s2 + 1));
 	if (ptr == NULL)
 		return (NULL);
-	ft_memcpy(ptr, s1, len_s1);
-	ft_memcpy(ptr + len_s1, s2, len_s2);
+	i = 0;
+	j = 0;
+	while (len_s1 - j)
+		ptr[i++] = s1[j++];
+	j = 0;
+	while (len_s2 - j)
+		ptr[i++] = s2[j++];
 	ptr[len_s1 + len_s2] = '\0';
 	return (ptr);
 }
@@ -99,25 +87,27 @@ char	*ft_strdup(const char *s)
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*ptr;
+	char			*src;
 	size_t			len_s;
+	int				i;
 
 	if (s == NULL)
 		return (NULL);
 	len_s = ft_strlen(s);
 	if (start >= len_s)
-	{
-		ptr = malloc(sizeof (char) * 1);
-		if (ptr == NULL)
-			return (NULL);
-		ptr[0] = '\0';
-		return (ptr);
-	}
+		return (ft_strdup(""));
 	if (len > len_s - start)
 		len = len_s - start;
 	ptr = (char *)malloc(sizeof (char) * (len + 1));
 	if (ptr == NULL)
 		return (NULL);
-	ft_memcpy(ptr, &s[start], len);
+	src = (char *)&s[start];
+	i = 0;
+	while (len - i > 0)
+	{
+		ptr[i] = src[i];
+		i++;
+	}
 	ptr[len] = '\0';
 	return (ptr);
 }
